c8ce5720d8d9c72338f2b335a42c63bc493ea9c2
nicole-fsd <nlf4@case.edu>
2020-05-28 23:33:49 +0200

    connect db, ad entities

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,13 +5,12 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use \Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ApiResource()
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User implements UserInterface
+class User
 {
     /**
      * @ORM\Id()
@@ -27,14 +26,23 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     *
      */
     private $password;
 
     /**
-     * @ORM\Column(type="json", nullable=true)
+     * @ORM\Column(type="string", length=255)
      */
-    private $roles = array();
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $age;
 
     public function getId(): ?int
     {
@@ -53,27 +61,6 @@ class User implements UserInterface
         return $this;
     }
 
-    public function getUsername(): string
-    {
-        return (string) $this->email;
-    }
-
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
-
-        return array_unique($roles);
-    }
-
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
-
-        return $this;
-    }
-
     public function getPassword(): ?string
     {
         return $this->password;
@@ -86,11 +73,39 @@ class User implements UserInterface
         return $this;
     }
 
-    public function getSalt()
+    public function getFirstname(): ?string
     {
+        return $this->firstname;
     }
 
-    public function eraseCredentials()
+    public function setFirstname(string $firstname): self
     {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(?int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
     }
 }
